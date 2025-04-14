f8b995132e9f953cd45a12bd27dd4c5b7e05ba5e
David Barley <ghuitster@gmail.com>
2025-04-14 00:26:05 -0400

    Single quotes instead of double

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,15 +1,15 @@
 AUTHOR = 'David'
 SITENAME = 'David Barley'
 SITESUBTITLE = 'About, Resume, and Blog'
-SITEURL = ""
+SITEURL = ''
 
-PATH = "content"
+PATH = 'content'
 
 TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = 'en'
 
-THEME = "./theme/gum"
+THEME = './theme/gum'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -28,10 +28,10 @@ EXTRA_PATH_METADATA = {
 
 # Social widget
 SOCIAL = (
-    ("Bluesky", "https://bsky.app/profile/davidmbarley.com"),
-    ("Facebook", "https://www.facebook.com/david.m.barley"),
-    ("LinkedIn", "https://www.linkedin.com/in/david-barley-7629367b/"),
-    ("GitHub", "https://github.com/ghuitster"),
+    ('Bluesky', 'https://bsky.app/profile/davidmbarley.com'),
+    ('Facebook', 'https://www.facebook.com/david.m.barley'),
+    ('LinkedIn', 'https://www.linkedin.com/in/david-barley-7629367b/'),
+    ('GitHub', 'https://github.com/ghuitster'),
 )
 
 DEFAULT_PAGINATION = 10
